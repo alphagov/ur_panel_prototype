6e43683e5a63047808b2f695d5dc4df95da4ed88
Tim Paul <tim.paul@digital.cabinet-office.gov.uk>
2015-06-11 08:28:08 +0100

    refactor file structure and add 3rd version

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,11 +2,11 @@ $(document).ready(function(){
 
   GOVUK.toggle.init();
 
-
+  // Initialise auto-suggest fields
   $('.auto-suggest').selectToAutocomplete();
 
-
-  var $buttons = $(".apply-1 label input[type='radio'], .apply-1 label input[type='checkbox']");
+  // Selected state on grey form control labels
+  var $buttons = $(".version-1 label input[type='radio'], .version-1 label input[type='checkbox']");
   var selectionButtons = new GOVUK.SelectionButtons($buttons);
 
 });
--- app/routes.js
+++ app/routes.js
@@ -12,39 +12,39 @@ module.exports = {
     // ROUTES - V1
 
     // Age
-    app.get('/apply-1/age', function (req, res) {
+    app.get('/version-1/age', function (req, res) {
     	switch (req.query['age16']){
-      		case 'age16-1'	: 	res.redirect('apply-1/contact-details');
-      		case 'age16-2'	:  	res.redirect('apply-1/ineligible');
-      		default			:   res.render('apply-1/age');
+      		case 'age16-1'	: 	res.redirect('version-1/contact-details');
+      		case 'age16-2'	:  	res.redirect('version-1/ineligible');
+      		default			:   res.render('version-1/age');
       	}
     });
 
     // English first language
-    app.get('/apply-1/speak-english', function (req, res) {
+    app.get('/version-1/speak-english', function (req, res) {
       switch (req.query['english']){
-          case 'english-yes'  :  res.redirect('apply-1/housing');
-          case 'english-no'  :   res.redirect('apply-1/speak-english-confidence');
-          default     :   res.render('apply-1/speak-english');
+          case 'english-yes'  :  res.redirect('version-1/housing');
+          case 'english-no'  :   res.redirect('version-1/speak-english-confidence');
+          default     :   res.render('version-1/speak-english');
         }
     });
 
     // Children under 18
-    app.get('/apply-1/children-under-18', function (req, res) {
+    app.get('/version-1/children-under-18', function (req, res) {
       switch (req.query['children-under-18']){
-          case 'children-under-18-yes'  :  res.redirect('apply-1/children-age');
-          case 'children-under-18-no'  :   res.redirect('apply-1/employment');
-          default     :   res.render('apply-1/children-under-18');
+          case 'children-under-18-yes'  :  res.redirect('version-1/children-age');
+          case 'children-under-18-no'  :   res.redirect('version-1/employment');
+          default     :   res.render('version-1/children-under-18');
         }
     });
     
     // Employment
-    app.get('/apply-1/confirmation', function (req, res) {
+    app.get('/version-1/confirmation', function (req, res) {
 
       if (req.query['employment-1'] === "on" || req.query['employment-2'] === "on" || req.query['employment-3'] === "on"){
-        res.redirect('apply-1/employment-sector');
+        res.redirect('version-1/employment-sector');
       } else {
-        res.render('apply-1/confirmation');
+        res.render('version-1/confirmation');
       }
 
     });
@@ -54,42 +54,54 @@ module.exports = {
     // ROUTES - V2
  
     // Age
-    app.get('/apply-2/age', function (req, res) {
+    app.get('/version-2/age', function (req, res) {
       switch (req.query['age16']){
-          case 'age16-1'  :   res.redirect('apply-2/contact-details');
-          case 'age16-2'  :   res.redirect('apply-2/ineligible');
-          default     :   res.render('apply-2/age');
+          case 'age16-1'  :   res.redirect('version-2/contact-details');
+          case 'age16-2'  :   res.redirect('version-2/ineligible');
+          default     :   res.render('version-2/age');
         }
     });
 
     // English first language
-    app.get('/apply-2/speak-english', function (req, res) {
+    app.get('/version-2/speak-english', function (req, res) {
       switch (req.query['english']){
-          case 'english-yes'  :  res.redirect('apply-2/housing');
-          case 'english-no'  :   res.redirect('apply-2/speak-english-confidence');
-          default     :   res.render('apply-2/speak-english');
+          case 'english-yes'  :  res.redirect('version-2/housing');
+          case 'english-no'  :   res.redirect('version-2/speak-english-confidence');
+          default     :   res.render('version-2/speak-english');
         }
     });
 
     // Children under 18
-    app.get('/apply-2/children-under-18', function (req, res) {
+    app.get('/version-2/children-under-18', function (req, res) {
       switch (req.query['children-under-18']){
-          case 'children-under-18-yes'  :  res.redirect('apply-2/children-age');
-          case 'children-under-18-no'  :   res.redirect('apply-2/employment');
-          default     :   res.render('apply-2/children-under-18');
+          case 'children-under-18-yes'  :  res.redirect('version-2/children-age');
+          case 'children-under-18-no'  :   res.redirect('version-2/employment');
+          default     :   res.render('version-2/children-under-18');
         }
     });
     
     // Employment
-    app.get('/apply-2/confirmation', function (req, res) {
+    app.get('/version-2/confirmation', function (req, res) {
 
       if (req.query['employment-1'] === "on" || req.query['employment-2'] === "on" || req.query['employment-3'] === "on"){
-        res.redirect('apply-2/employment-sector');
+        res.redirect('version-2/employment-sector');
       } else {
-        res.render('apply-2/confirmation');
+        res.render('version-2/confirmation');
       }
 
     });   
+
+
+    // ROUTES - V3
+ 
+    // Age
+    app.get('/version-3/age', function (req, res) {
+      switch (req.query['age16']){
+          case 'age16-1'  :   res.redirect('version-3/contact-details');
+          case 'age16-2'  :   res.redirect('version-3/ineligible');
+          default     :   res.render('version-3/age');
+        }
+    }); 
 
   }
 };
